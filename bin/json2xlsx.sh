7c7e867030226e30c147c3af309a76f88434be62
Chris Moreton <chris@netsensia.com>
2019-08-22 17:08:51 +0100

    Add dev version to json2xlsx

--- bin/json2xlsx.sh
+++ bin/json2xlsx.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-docker build --no-cache -t hmctspublic.azurecr.io/sscs/ccd-definition-importer:dev -f importer.Dockerfile .
+
+DEFINITION_VERSION=dev
+
+docker build --no-cache -t hmctspublic.azurecr.io/sscs/ccd-definition-importer:$DEFINITION_VERSION -f importer.Dockerfile .
 docker run -ti --rm --name json2xlsx \
   -v $(pwd)/releases:/tmp \
-  hmctspublic.azurecr.io/sscs/ccd-definition-importer:dev \
-  sh -c "cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /tmp/sscs-ccd.xlsx"
+  hmctspublic.azurecr.io/sscs/ccd-definition-importer:$DEFINITION_VERSION \
+  sh -c "cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /tmp/sscs-ccd-$DEFINITION_VERSION.xlsx"
