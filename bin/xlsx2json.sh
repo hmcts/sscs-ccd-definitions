e4a387140e7046bdfedacdd3ca85480c76510856
Chris Moreton <chris@netsensia.com>
2019-09-11 16:32:32 +0100

    Remove Change History tab

--- bin/xlsx2json.sh
+++ bin/xlsx2json.sh
@@ -12,3 +12,4 @@ docker build --no-cache -f ../docker/xlsx2json.Dockerfile -t xlsx2json .
 echo `pwd`
 docker run -v `pwd`/data:/data xlsx2json
 ../bin/template-urls.sh
+rm data/sheets/Change\ History.json
