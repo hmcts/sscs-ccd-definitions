e75b57b58be1c6adc43588b44cd2f95994f037ed
Chris Moreton <chris@netsensia.com>
2019-09-12 13:52:57 +0100

    Manage FixedLists between AAT and PROD

--- bin/template-urls.sh
+++ bin/template-urls.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
+SHEETS_DIR=`pwd`/data/sheets
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/http:\/\/em-ccd-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_EM_CCD_ORCHESTRATOR_URL}/g' `pwd`/data/sheets/*.json
-  sed -i '' 's/http:\/\/sscs-ccd-callback-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL}/g' `pwd`/data/sheets/*.json
-  sed -i '' 's/http:\/\/sscs-tribunals-api-aat.service.core-compute-aat.internal/\${CCD_DEF_TRIBUNALS_API_URL}/g' `pwd`/data/sheets/*.json
-  sed -i '' 's/http:\/\/sscs-tya-notif-aat.service.core-compute-aat.internal/\${CCD_DEF_TYA_NOTIFICATIONS_API_URL}/g' `pwd`/data/sheets/*.json
-  sed -i '' 's/http:\/\/sscs-bulk-scan-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_API_URL}/g' `pwd`/data/sheets/*.json
-  sed -i '' 's/http:\/\/bulk-scan-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL}/g' `pwd`/data/sheets/*.json
-  sed -i '' 's/http:\/\/sscs-cor-backend-aat.service.core-compute-aat.internal/\${CCD_DEF_COR_BACKEND_URL}/g' `pwd`/data/sheets/*.json
+  sed -i '' 's/http:\/\/em-ccd-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_EM_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i '' 's/http:\/\/sscs-ccd-callback-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i '' 's/http:\/\/sscs-tribunals-api-aat.service.core-compute-aat.internal/\${CCD_DEF_TRIBUNALS_API_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i '' 's/http:\/\/sscs-tya-notif-aat.service.core-compute-aat.internal/\${CCD_DEF_TYA_NOTIFICATIONS_API_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i '' 's/http:\/\/sscs-bulk-scan-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_API_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i '' 's/http:\/\/bulk-scan-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i '' 's/http:\/\/sscs-cor-backend-aat.service.core-compute-aat.internal/\${CCD_DEF_COR_BACKEND_URL}/g' ${SHEETS_DIR}/*.json
 else
-  sed -i 's/http:\/\/em-ccd-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_EM_CCD_ORCHESTRATOR_URL}/g' `pwd`/data/sheets/*.json
-  sed -i 's/http:\/\/sscs-ccd-callback-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL}/g' `pwd`/data/sheets/*.json
-  sed -i 's/http:\/\/sscs-tribunals-api-aat.service.core-compute-aat.internal/\${CCD_DEF_TRIBUNALS_API_URL}/g' `pwd`/data/sheets/*.json
-  sed -i 's/http:\/\/sscs-tya-notif-aat.service.core-compute-aat.internal/\${CCD_DEF_TYA_NOTIFICATIONS_API_URL}/g' `pwd`/data/sheets/*.json
-  sed -i 's/http:\/\/sscs-bulk-scan-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_API_URL}/g' `pwd`/data/sheets/*.json
-  sed -i 's/http:\/\/bulk-scan-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL}/g' `pwd`/data/sheets/*.json
-  sed -i 's/http:\/\/sscs-cor-backend-aat.service.core-compute-aat.internal/\${CCD_DEF_COR_BACKEND_URL}/g' `pwd`/data/sheets/*.json
-  fi
+  sed -i 's/http:\/\/em-ccd-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_EM_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i 's/http:\/\/sscs-ccd-callback-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i 's/http:\/\/sscs-tribunals-api-aat.service.core-compute-aat.internal/\${CCD_DEF_TRIBUNALS_API_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i 's/http:\/\/sscs-tya-notif-aat.service.core-compute-aat.internal/\${CCD_DEF_TYA_NOTIFICATIONS_API_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i 's/http:\/\/sscs-bulk-scan-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_API_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i 's/http:\/\/bulk-scan-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
+  sed -i 's/http:\/\/sscs-cor-backend-aat.service.core-compute-aat.internal/\${CCD_DEF_COR_BACKEND_URL}/g' ${SHEETS_DIR}/*.json
+fi
+
+# Remove all AssignTo lines and replace with a template var
+sed '/FL_AssignTo/d' ${SHEETS_DIR}/FixedLists.json | sed 's/\[/\[${CCD_DEF_FIXED_LIST_USERS},/' > ${SHEETS_DIR}/FixedLists_Temp.json
+cp ${SHEETS_DIR}/FixedLists_Temp.json ${SHEETS_DIR}/FixedLists.json
+rm ${SHEETS_DIR}/FixedLists_Temp.json
+
+# Remove comma from last row, if it exists
+sed "$(( $( wc -l < ${SHEETS_DIR}/FixedLists.json) -1 ))s/,$//" ${SHEETS_DIR}/FixedLists.json
