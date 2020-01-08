df4dc7727f7990c6c775c0fc111958894fbc3530
jackmaloney <jack.maloney@transformuk.com>
2020-01-08 13:48:03 +0000

    Set env automatically

--- bin/create-xlsx.sh
+++ bin/create-xlsx.sh
@@ -51,6 +51,7 @@ case ${ENV} in
   aat|demo|local)
     TYA_LINK="https://sscs-tya-frontend-aat.service.core-compute-aat.internal/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
     TYA_APPOINTEE_LINK="https://sscs-tya-frontend-aat.service.core-compute-aat.internal/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+    CCD_ENV=${ENV}
   ;;
   prod)
     TYA_LINK="https://www.track-benefit-appeal.service.gov.uk/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
@@ -87,6 +88,6 @@ docker run -ti --rm --name json2xlsx \
   -e "CCD_DEF_TYA_LINK=${TYA_LINK}" \
   -e "CCD_DEF_TYA_APPOINTEE_LINK=${TYA_APPOINTEE_LINK}" \
   -e "CCD_DEF_FIXED_LIST_USERS=${FIXED_LIST_USERS}" \
-  -e "CCD_ENV=${FIXED_LISTS_SUFFIX}" \
+  -e "CCD_ENV=${UPPERCASE_ENV}" \
   hmctspublic.azurecr.io/sscs/ccd-definition-importer-${TYPE}:${VERSION} \
   sh -c "cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /tmp/CCD_${CASE_TYPE_XLSX_NAME}Definition_v${VERSION}_${UPPERCASE_ENV}.xlsx"
