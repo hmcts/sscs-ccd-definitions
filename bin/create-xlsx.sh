434fc368e8adcbbbfb3ed7e951edf881a5f9094f
jackmaloney <jack.maloney@transformuk.com>
2020-07-07 13:37:09 +0100

    SSCS-7627 Build hearing dropdown for next hearing screen

--- bin/create-xlsx.sh
+++ bin/create-xlsx.sh
@@ -101,6 +101,7 @@ fi
 if [ ${TYPE} == "benefit" ]; then
   FIXED_LIST_USERS=$(cat ${RUN_DIR}/${TYPE}/FixedLists_AssignTo_${FIXED_LISTS_SUFFIX}.txt)
   DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/decision-notice-questions.txt)
+  source build-venue-list.sh
 else
   FIXED_LIST_USERS=" "
 fi
@@ -123,6 +124,7 @@ docker run -ti --rm --name json2xlsx \
   -e "CCD_DEF_MYA_APPOINTEE_LINK=${MYA_APPOINTEE_LINK}" \
   -e "CCD_DEF_FIXED_LIST_USERS=${FIXED_LIST_USERS}" \
   -e "CCD_DEF_DECISION_NOTICE_QUESTIONS=${DECISION_NOTICE_QUESTIONS}" \
+  -e "CCD_DEF_VENUE_LIST=${VENUE_LIST}" \
   -e "CCD_DEF_E=${UPPERCASE_ENV}" \
   hmctspublic.azurecr.io/sscs/ccd-definition-importer-${TYPE}:${VERSION} \
   sh -c "cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /tmp/CCD_${CASE_TYPE_XLSX_NAME}Definition_v${VERSION}_${UPPERCASE_ENV}.xlsx"
