1a445f65c2e396cc65d23ada49877b0174fbfd6b
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-08-25 10:19:02 +0100

    enableHighLevelDataSetup step in Jenkins

---
+++ src/test/java/benefit/HighLevelDataSetupApp.java
@@ -0,0 +1,20 @@
+package benefit;
+
+import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+
+public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
+
+    public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
+        super(dataSetupEnvironment);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        main(HighLevelDataSetupApp.class, args);
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        return true;
+    }
+}
