2b6556f0316d940132a830865f1bb818c722b911
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-09-02 14:56:26 +0100

    Build sscs-ccd-definition-importer image and create definition xlsx file to import in highLevelDataSetup

---
+++ bin/before-data-setup-step.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+TYPE=${1}
+ENV=${2}
+LIKE_PROD=${3:-${ENV}}
+
+RUN_DIR=`pwd`
+COMMON_VERSION=$(cat ${RUN_DIR}/benefit/SSCS_COMMON_VERSION.txt)
+
+if [ -z "${TYPE}" ] || [ -z "${ENV}" ]; then
+    echo "Usage create-xlsx.sh [type] [env]"
+    exit 1
+fi
+
+if [ ${ENV} == "preview" ]; then
+    ENV="aat"
+fi
+
+REPOSITORY=sscs/ccd-definitions
+ACR_NAME=hmctspublic
+TIMESTAMP=$(printf '%s\n' "$LAST_COMMIT_TIMESTAMP")
+
+LATEST_TAG=$(az acr repository show-tags -n $ACR_NAME --repository $REPOSITORY | grep $TIMESTAMP)
+echo "Latest tag from repo $LATEST_TAG"
+
+case ${TYPE} in
+    "benefit" )
+        CASE_TYPE_XLSX_NAME="SSCS" ;;
+    "bulkscan" )
+        CASE_TYPE_XLSX_NAME="BulkScanning" ;;
+    * )
+        echo "Type must be benefit or bulkscan"
+        exit 1
+esac
+
+if [ ${ENV} == "local" ]; then
+    EM_CCD_ORCHESTRATOR_URL="http://host.docker.internal:4623"
+    SSCS_CCD_ORCHESTRATOR_URL="http://host.docker.internal:8070"
+    TRIBUNALS_API_URL="http://host.docker.internal:8080"
+    TYA_NOTIFICATIONS_API_URL="http://host.docker.internal:8081"
+    BULK_SCAN_API_URL="http://host.docker.internal:8090"
+    BULK_SCAN_ORCHESTRATOR_URL="http://host.docker.internal:8099"
+elif [ ${ENV} == "aat" ] || [ ${ENV} == "demo" ] || [ ${ENV} == "prod" ] || [ ${ENV} == "perftest" ] || [ ${ENV} == "ithc" ]; then
+    SSCS_CCD_ORCHESTRATOR_URL="http://sscs-ccd-callback-orchestrator-${ENV}.service.core-compute-${ENV}.internal"
+    TRIBUNALS_API_URL="http://sscs-tribunals-api-${ENV}.service.core-compute-${ENV}.internal"
+    TYA_NOTIFICATIONS_API_URL="http://sscs-tya-notif-${ENV}.service.core-compute-${ENV}.internal"
+    BULK_SCAN_API_URL="http://sscs-bulk-scan-${ENV}.service.core-compute-${ENV}.internal"
+    BULK_SCAN_ORCHESTRATOR_URL="http://sscs-bulk-scan-orchestrator-${ENV}.service.core-compute-${ENV}.internal"
+else
+        echo "${ENV} not recognised"
+        exit 1
+fi
+
+if [ ${ENV} == "demo" ] || [ ${ENV} == "ithc" ]; then
+    EM_CCD_ORCHESTRATOR_URL="http://em-ccdorc-demo.service.core-compute-demo.internal/"
+elif [ ${ENV} == "aat" ] || [ ${ENV} == "perftest" ] || [ ${ENV} == "prod" ]; then
+    EM_CCD_ORCHESTRATOR_URL="http://em-ccd-orchestrator-${ENV}.service.core-compute-${ENV}.internal"
+fi
+
+case ${ENV} in
+  local)
+    MYA_LINK="http://dockerhost:3000/sign-in?tya=\${subscriptions.appellantSubscription.tya}"
+    MYA_REPRESENTATIVE_LINK="http://dockerhost:3000/sign-in?tya=\${subscriptions.representativeSubscription.tya}"
+    MYA_APPOINTEE_LINK="http://dockerhost:3000/sign-in?tya=\${subscriptions.appointeeSubscription.tya}"
+    TYA_LINK="http://dockerhost:3000/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
+    TYA_APPOINTEE_LINK="http://dockerhost:3000/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+  ;;
+  demo)
+    TYA_LINK="https://sscs-tya-frontend-${ENV}.service.core-compute-${ENV}.internal/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
+    TYA_APPOINTEE_LINK="https://sscs-tya-frontend-${ENV}.service.core-compute-${ENV}.internal/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+    MYA_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.appellantSubscription.tya}"
+    MYA_REPRESENTATIVE_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.representativeSubscription.tya}"
+    MYA_APPOINTEE_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.appointeeSubscription.tya}"
+  ;;
+  perftest)
+    TYA_LINK="https://sscs-tya-frontend-${ENV}.service.core-compute-${ENV}.internal/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
+    TYA_APPOINTEE_LINK="https://sscs-tya-frontend-${ENV}.service.core-compute-${ENV}.internal/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+    MYA_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.appellantSubscription.tya}"
+    MYA_REPRESENTATIVE_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.representativeSubscription.tya}"
+    MYA_APPOINTEE_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.appointeeSubscription.tya}"
+  ;;
+  ithc)
+    TYA_LINK="https://sscs-tya-frontend-${ENV}.service.core-compute-${ENV}.internal/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
+    TYA_APPOINTEE_LINK="https://sscs-tya-frontend-${ENV}.service.core-compute-${ENV}.internal/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+    MYA_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.appellantSubscription.tya}"
+    MYA_REPRESENTATIVE_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.representativeSubscription.tya}"
+    MYA_APPOINTEE_LINK="http://sscs-cor-frontend-${ENV}.service.core-compute-${ENV}.internal/sign-in?tya=\${subscriptions.appointeeSubscription.tya}"
+  ;;
+  aat)
+    TYA_LINK="http://track-appeal.aat.platform.hmcts.net/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
+    TYA_APPOINTEE_LINK="http://track-appeal.aat.platform.hmcts.net/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+    MYA_LINK="http://sscs-cor.aat.platform.hmcts.net/sign-in?tya=\${subscriptions.appellantSubscription.tya}"
+    MYA_REPRESENTATIVE_LINK="http://sscs-cor.aat.platform.hmcts.net/sign-in?tya=\${subscriptions.representativeSubscription.tya}"
+    MYA_APPOINTEE_LINK="http://sscs-cor.aat.platform.hmcts.net/sign-in?tya=\${subscriptions.appointeeSubscription.tya}"
+    ;;
+  prod)
+    TYA_LINK="https://www.track-benefit-appeal.service.gov.uk/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal"
+    TYA_APPOINTEE_LINK="https://www.track-benefit-appeal.service.gov.uk/validate-surname/\${subscriptions.appointeeSubscription.tya}/trackyourappeal"
+    MYA_LINK="https://www.manage.appeal-benefit-decision.service.gov.uk/sign-in?tya=\${subscriptions.appellantSubscription.tya}"
+    MYA_REPRESENTATIVE_LINK="https://www.manage.appeal-benefit-decision.service.gov.uk/sign-in?tya=\${subscriptions.representativeSubscription.tya}"
+    MYA_APPOINTEE_LINK="https://www.manage.appeal-benefit-decision.service.gov.uk/sign-in?tya=\${subscriptions.appointeeSubscription.tya}"
+
+  ;;
+  *)
+    echo "${ENV} not recognised"
+    exit 1 ;;
+esac
+
+if [ ${ENV} == "prod" ]; then
+    FIXED_LISTS_SUFFIX="PROD"
+else
+    FIXED_LISTS_SUFFIX="AAT"
+fi
+
+if [ ${TYPE} == "benefit" ]; then
+  PIP_DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/pip-decision-notice-questions.txt)
+  ESA_DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/esa-decision-notice-questions.txt)
+  UC_DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/uc-decision-notice-questions.txt)
+  LANGUAGES=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/languages.txt)
+fi
+
+UPPERCASE_ENV=$(printf '%s\n' "${ENV}" | awk '{ print toupper($0) }')
+
+if [ ${ENV} == "prod" ] || [ ${LIKE_PROD} == "prod" ]; then
+  excludedFilenamePatterns="-e *-nonprod.json"
+else
+  excludedFilenamePatterns="-e *-prod.json"
+fi
+
+if [ ${ENV} == "prod" ] || [ ${LIKE_PROD} == "prod" ]; then
+  echo "${ENV} deployment is after all the testing done. Remove this condition after test on master"
+  exit 1
+else
+  docker run -i --rm --name json2xlsx \
+    -v $(pwd)/src/test/resources/ccd_definition:/tmp \
+    -e "CCD_DEF_EM_CCD_ORCHESTRATOR_URL=${EM_CCD_ORCHESTRATOR_URL}" \
+    -e "CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL=${SSCS_CCD_ORCHESTRATOR_URL}" \
+    -e "CCD_DEF_TRIBUNALS_API_URL=${TRIBUNALS_API_URL}" \
+    -e "CCD_DEF_TYA_NOTIFICATIONS_API_URL=${TYA_NOTIFICATIONS_API_URL}" \
+    -e "CCD_DEF_BULK_SCAN_API_URL=${BULK_SCAN_API_URL}" \
+    -e "CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL=${BULK_SCAN_ORCHESTRATOR_URL}" \
+    -e "CCD_DEF_TYA_LINK=${TYA_LINK}" \
+    -e "CCD_DEF_TYA_APPOINTEE_LINK=${TYA_APPOINTEE_LINK}" \
+    -e "CCD_DEF_MYA_LINK=${MYA_LINK}" \
+    -e "CCD_DEF_MYA_REPRESENTATIVE_LINK=${MYA_REPRESENTATIVE_LINK}" \
+    -e "CCD_DEF_MYA_APPOINTEE_LINK=${MYA_APPOINTEE_LINK}" \
+    -e "CCD_DEF_PIP_DECISION_NOTICE_QUESTIONS=${PIP_DECISION_NOTICE_QUESTIONS}" \
+    -e "CCD_DEF_ESA_DECISION_NOTICE_QUESTIONS=${ESA_DECISION_NOTICE_QUESTIONS}" \
+    -e "CCD_DEF_UC_DECISION_NOTICE_QUESTIONS=${UC_DECISION_NOTICE_QUESTIONS}" \
+    -e "CCD_DEF_LANGUAGES=${LANGUAGES}" \
+    -e "CCD_DEF_E=${UPPERCASE_ENV}" \
+    hmctspublic.azurecr.io/sscs/ccd-definitions:${LATEST_TAG} \
+    sh -c "cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets ${excludedFilenamePatterns} -o /tmp/CCD_${CASE_TYPE_XLSX_NAME}Definition_${UPPERCASE_ENV}.xlsx"
+fi
--- src/test/java/benefit/HighLevelDataSetupApp.java
+++ src/test/java/benefit/HighLevelDataSetupApp.java
@@ -3,18 +3,45 @@ package benefit;
 import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 
+import java.util.Arrays;
+
+
 public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
 
+    private static final String definitionsPath = "ccd_definition";
+
     public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
-        super(dataSetupEnvironment);
+        super(dataSetupEnvironment, definitionsPath);
     }
 
     public static void main(String[] args) throws Throwable {
+        //TODO remove this when prod deployment reddy
+        skipProd(args);
+
         main(HighLevelDataSetupApp.class, args);
+    }
+
+    private static void skipProd(String[] args) {
+        if (args.length >= 1) {
+            try {
+                CcdEnvironment environment = CcdEnvironment.valueOf(args[0].toUpperCase());
+                if (CcdEnvironment.PROD.equals(environment)) {
+                    throw new IllegalArgumentException(
+                            "Prod is not ready yet");
+                }
+            } catch (IllegalArgumentException e) {
+            }
+        }
+
     }
 
     @Override
     protected boolean shouldTolerateDataSetupFailure() {
         return true;
+    }
+
+    @Override
+    protected void doLoadTestData() {
+        importDefinitions();
     }
 }
