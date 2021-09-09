10e3e0fa6374556010f540c489d53b529f039243
jackmaloney <jack.maloney@transformuk.com>
2021-09-09 10:12:11 +0100

    SSCS-9538 distinguish between hmrc and dwp cases

--- benefit/data/other-benefit.sh
+++ benefit/data/other-benefit.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-directories=("CaseField" "CaseTypeTab" "State" "CaseEvent" "CaseEventToFields" "SearchInputFields" "SearchResultFields" "WorkBasketInputFields" "WorkBasketResultFields" "AuthorisationCaseField" "AuthorisationCaseEvent" "AuthorisationCaseState")
+directories=("CaseField" "CaseTypeTab" "State" "CaseEvent" "CaseEventToFields" "SearchInputFields" "SearchResultFields" "WorkBasketInputFields" "WorkBasketResultFields" "AuthorisationCaseType" "AuthorisationCaseField" "AuthorisationCaseEvent" "AuthorisationCaseState")
 
 search_dir="/data/sheets"
+
+STRING="manual"
 
 for tab in "${directories[@]}"
 do
   for file in ${search_dir}/${tab}/*
   do
-    if [[ -f $file ]]; then
+    if [[ ( "$file" != *"$STRING"* ) && ( -f $file ) ]]; then
         #copy stuff ....
         jq '.[].CaseTypeID = "OtherBenefit"' ${file} > /tmp/tmp.json
         jq -s '[.[][]]' ${file} /tmp/tmp.json > /tmp/tmp2.json
