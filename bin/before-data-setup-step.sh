bb517583c4abceedfeb09429e4d817b0f4b622c4
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-10-28 10:21:12 +0100

    is the subscription name incorrect, use an id instead

--- bin/before-data-setup-step.sh
+++ bin/before-data-setup-step.sh
@@ -19,7 +19,7 @@ fi
 #TIMESTAMP=$(printf '%s\n' "$LAST_COMMIT_TIMESTAMP")
 #SUBSCRIPTION=$(printf '%s\n' "$REGISTRY_SUBSCRIPTION")
 
-az acr login --name hmctspublic --subscription DCD-CFTAPPS-DEV
+az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 LATEST_TAG=$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --orderby time_desc --top 5| grep $BRANCH_NAME| head -n 1| sed 's/"//g;s/,//g;s/ //g')
 
 
