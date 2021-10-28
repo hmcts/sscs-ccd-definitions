4aea094dd251f60f1d3d6edfa470dd94c88de7b1
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-10-28 15:26:04 +0100

    remove az login

--- bin/before-data-setup-step.sh
+++ bin/before-data-setup-step.sh
@@ -23,7 +23,6 @@ echo "SUBSCRIPTION is $SUBSCRIPTION"
 echo "BRANCH_NAME is $BRANCH_NAME"
 
 
-az login
 az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 LATEST_TAG=$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --orderby time_desc --top 5| grep $BRANCH_NAME| head -n 1| sed 's/"//g;s/,//g;s/ //g')
 
