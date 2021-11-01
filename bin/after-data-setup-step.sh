05e166ee75835af3258bcb70591dbb9900cff00c
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-11-01 15:50:46 +0000

    SSCS-9626 - Remove chown

--- bin/after-data-setup-step.sh
+++ bin/after-data-setup-step.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sudo chown -R jenkins:jenkins $(pwd)/src/test/resources
 mv $(pwd)/src/test/resources/ccd_definition/keepme ./
 rm -rf $(pwd)/src/test/resources/ccd_definition/* 2>/dev/null
 mv ./keepme $(pwd)/src/test/resources/ccd_definition/
