0ddb116d557d6bb5a642c2904cfe0c948fd63aaf
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-12-17 16:52:48 +0000

    remove double quotes

--- bin/before-data-setup-step.sh
+++ bin/before-data-setup-step.sh
@@ -28,9 +28,9 @@ echo "SUBSCRIPTION is $SUBSCRIPTION"
 #az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 #LATEST_TAG=$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --orderby time_desc -o tsv --query "[]")
 
-if [[ "$BRANCH_NAME" == "staging" ]]; then
+if [[ $BRANCH_NAME == "staging" ]]; then
   LATEST_TAG="$BRANCH_NAME-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
-elif [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "demo" || "$BRANCH_NAME" == "ithc" || "$BRANCH_NAME" == "perftest" ]]; then
+elif [[ $BRANCH_NAME == "master" || $BRANCH_NAME == "demo" || $BRANCH_NAME == "ithc" || $BRANCH_NAME == "perftest" ]]; then
    LATEST_TAG="aat-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
 else
   LATEST_TAG="This-is-probably-a-PR-so-fail-here-branch-name-is-${BRANCH_NAME}"
