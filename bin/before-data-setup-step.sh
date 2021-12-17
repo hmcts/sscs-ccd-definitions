97f536cd749070bc676ba9d2c47c5a25b5301395
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-12-17 17:05:26 +0000

    really bash?

--- bin/before-data-setup-step.sh
+++ bin/before-data-setup-step.sh
@@ -7,6 +7,10 @@ LIKE_PROD=${3:-${ENV}}
 RUN_DIR=`pwd`
 COMMON_VERSION=$(cat ${RUN_DIR}/benefit/SSCS_COMMON_VERSION.txt)
 
+if [[ "string" == "string" ]]; then
+    echo "This is Bash"
+fi
+
 if [ -z "${TYPE}" ] || [ -z "${ENV}" ]; then
     echo "Usage create-xlsx.sh [type] [env]"
     exit 1
@@ -28,7 +32,7 @@ echo "SUBSCRIPTION is $SUBSCRIPTION"
 #az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 #LATEST_TAG=$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --orderby time_desc -o tsv --query "[]")
 
-if [[ $BRANCH_NAME == "staging" ]]; then
+if [[ ${BRANCH_NAME} == "staging" ]]; then
   LATEST_TAG="$BRANCH_NAME-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
 elif [[ $BRANCH_NAME == "master" || $BRANCH_NAME == "demo" || $BRANCH_NAME == "ithc" || $BRANCH_NAME == "perftest" ]]; then
    LATEST_TAG="aat-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
