b41e8539710427230d41b66f55d08112f262878c
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-12-17 16:29:02 +0000

    double brackets

--- bin/before-data-setup-step.sh
+++ bin/before-data-setup-step.sh
@@ -12,7 +12,7 @@ if [ -z "${TYPE}" ] || [ -z "${ENV}" ]; then
     exit 1
 fi
 
-if [ ${ENV} == "preview" ]; then
+if [[ ${ENV} == "preview" ]]; then
     ENV="aat"
 fi
 
@@ -28,7 +28,7 @@ echo "SUBSCRIPTION is $SUBSCRIPTION"
 #az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 #LATEST_TAG=$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --orderby time_desc -o tsv --query "[]")
 
-if [ "$BRANCH_NAME" == "staging" ]; then
+if [[ "$BRANCH_NAME" == "staging" ]]; then
   LATEST_TAG="$BRANCH_NAME-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
 elif [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "demo" || "$BRANCH_NAME" == "ithc" || "$BRANCH_NAME" == "perftest" ]]; then
    LATEST_TAG="aat-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
@@ -47,14 +47,14 @@ case ${TYPE} in
         exit 1
 esac
 
-if [ ${ENV} == "local" ]; then
+if [[ ${ENV} == "local" ]]; then
     EM_CCD_ORCHESTRATOR_URL="http://host.docker.internal:4623"
     SSCS_CCD_ORCHESTRATOR_URL="http://host.docker.internal:8070"
     TRIBUNALS_API_URL="http://host.docker.internal:8080"
     TYA_NOTIFICATIONS_API_URL="http://host.docker.internal:8081"
     BULK_SCAN_API_URL="http://host.docker.internal:8090"
     BULK_SCAN_ORCHESTRATOR_URL="http://host.docker.internal:8099"
-elif [ ${ENV} == "aat" ] || [ ${ENV} == "demo" ] || [ ${ENV} == "prod" ] || [ ${ENV} == "perftest" ] || [ ${ENV} == "ithc" ]; then
+elif [[ ${ENV} == "aat" || ${ENV} == "demo" || ${ENV} == "prod" || ${ENV} == "perftest" || ${ENV} == "ithc" ]]; then
     SSCS_CCD_ORCHESTRATOR_URL="http://sscs-ccd-callback-orchestrator-${ENV}.service.core-compute-${ENV}.internal"
     TRIBUNALS_API_URL="http://sscs-tribunals-api-${ENV}.service.core-compute-${ENV}.internal"
     TYA_NOTIFICATIONS_API_URL="http://sscs-tya-notif-${ENV}.service.core-compute-${ENV}.internal"
@@ -65,9 +65,9 @@ else
         exit 1
 fi
 
-if [ ${ENV} == "demo" ] || [ ${ENV} == "ithc" ]; then
+if [[ ${ENV} == "demo" || ${ENV} == "ithc" ]]; then
     EM_CCD_ORCHESTRATOR_URL="http://em-ccdorc-demo.service.core-compute-demo.internal/"
-elif [ ${ENV} == "aat" ] || [ ${ENV} == "perftest" ] || [ ${ENV} == "prod" ]; then
+elif [[ ${ENV} == "aat" || ${ENV} == "perftest" || ${ENV} == "prod" ]]; then
     EM_CCD_ORCHESTRATOR_URL="http://em-ccd-orchestrator-${ENV}.service.core-compute-${ENV}.internal"
 fi
 
@@ -120,13 +120,13 @@ case ${ENV} in
     exit 1 ;;
 esac
 
-if [ ${ENV} == "prod" ]; then
+if [[ ${ENV} == "prod" ]]; then
     FIXED_LISTS_SUFFIX="PROD"
 else
     FIXED_LISTS_SUFFIX="AAT"
 fi
 
-if [ ${TYPE} == "benefit" ]; then
+if [[ ${TYPE} == "benefit" ]]; then
   PIP_DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/pip-decision-notice-questions.txt)
   ESA_DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/esa-decision-notice-questions.txt)
   UC_DECISION_NOTICE_QUESTIONS=$(curl https://raw.githubusercontent.com/hmcts/sscs-common/$COMMON_VERSION/src/main/resources/reference-data/uc-decision-notice-questions.txt)
@@ -135,7 +135,7 @@ fi
 
 UPPERCASE_ENV=$(printf '%s\n' "${ENV}" | awk '{ print toupper($0) }')
 
-if [ ${ENV} == "prod" ] || [ ${LIKE_PROD} == "prod" ]; then
+if [[ ${ENV} == "prod" || ${LIKE_PROD} == "prod" ]]; then
   excludedFilenamePatterns="-e *-nonprod.json,*-shuttered.json"
 else
   excludedFilenamePatterns="-e *-prod.json,*-shuttered.json"
