d237cc1afdfecf0bf4fe2b28c4e487b5a6153af4
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-07-22 10:20:33 +0100

    sscsBaseChartUpdate-Node

--- bin/before-data-setup-step.sh
+++ bin/before-data-setup-step.sh
@@ -25,19 +25,15 @@ echo "TIMESTAMP is $TIMESTAMP"
 echo "BRANCH_NAME is $BRANCH_NAME"
 echo "SUBSCRIPTION is $SUBSCRIPTION"
 
-az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
-LATEST_TAG_FROM_ACR =$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --orderby time_desc -o tsv --query "[]")
-
-echo "LATEST_TAG_FROM_ACR"
-echo $LATEST_TAG_FROM_ACR
-echo "--------"
+#az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
+#LATEST_TAG=$(az acr repository show-tags -n hmctspublic --repository sscs/ccd-definitions --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --orderby time_desc -o tsv --query "[]")
 
 if [[ ${BRANCH_NAME} == "staging" ]]; then
   LATEST_TAG="$BRANCH_NAME-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
 elif [[ $BRANCH_NAME == "master" || $BRANCH_NAME == "demo" || $BRANCH_NAME == "ithc" || $BRANCH_NAME == "perftest" ]]; then
    LATEST_TAG="aat-${COMMIT_LABEL}-$LAST_COMMIT_TIMESTAMP"
 else
-  LATEST_TAG="This-is-probably-a-PR-so-fail-here-branch-name-is-${BRANCH_NAME}"
+  LATEST_TAG="This-is-probably-a-PR-so-fail-here-branch-name-is- ${BRANCH_NAME}"
 fi
 echo "Latest tag from repo $LATEST_TAG"
 
