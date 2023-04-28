05b7d9653fafa11d16d045e0da0276f1ea6bef34
Daniel Furnivall <danielfurnivall@gmail.com>
2023-04-28 17:44:21 +0100

    fix hooks better

--- e2e-tests/e2e/support/hooks.ts
+++ e2e-tests/e2e/support/hooks.ts
@@ -3,21 +3,30 @@ import { browser } from 'protractor';
 import { Logger } from '@hmcts/nodejs-logging';
 
 const logger = Logger.getLogger('hooks.ts');
+const LOG_LEVEL_ERROR_THRESHOLD = 900;
+const MIME_TYPE_PNG = 'image/png';
 
+// This hook runs after each scenario
 After(async function (scenario) {
   logger.info(`Scenario results are ${scenario.result.status}`);
-  if (scenario.result.status === Status.FAILED) {
-    const stream = await browser.takeScreenshot();
-    const decodedImage = Buffer.from(stream.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64');
-    await this.attach(decodedImage, 'image/png');
 
+  // If the scenario has failed, capture a screenshot and browser error logs
+  if (scenario.result.status === Status.FAILED) {
+    // Capture and attach a screenshot to the report
+    const screenshotStream = await browser.takeScreenshot();
+    const decodedImage = Buffer.from(screenshotStream.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64');
+    await this.attach(decodedImage, MIME_TYPE_PNG);
+
+    // Fetch browser logs and filter for errors
     const browserLog = await browser.manage().logs().get('browser');
     const browserErrorLogs = [];
-    for (const element of browserLog) {
-      if (element.level.value > 900) {
-        browserErrorLogs.push(element);
+    for (const logEntry of browserLog) {
+      if (logEntry.level.value > LOG_LEVEL_ERROR_THRESHOLD) {
+        browserErrorLogs.push(logEntry);
       }
     }
+
+    // Attach browser error logs to the report
     try {
       await this.attach(JSON.stringify(browserErrorLogs, null, 2));
     } catch (error) {
@@ -25,3 +34,4 @@ After(async function (scenario) {
     }
   }
 });
+
