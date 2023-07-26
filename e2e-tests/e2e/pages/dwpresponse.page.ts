9bde2fb570b675178e407678a2115ece5a775c52
Mohammed Ali <mohammed.ali@hmcts.net>
2023-07-26 14:30:15 +0100

    SSCS-11697 - Do not update the 'FTA Further Evidence State' or the FTA state if original sender is FTA

--- e2e-tests/e2e/pages/dwpresponse.page.ts
+++ e2e-tests/e2e/pages/dwpresponse.page.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { browser, by, element } from 'protractor';
 import { AnyPage } from './any.page';
 import { AnyCcdFormPage } from './any-ccd-form.page';
@@ -5,6 +6,7 @@ import { generateNINumber } from '../helpers/ni-generator';
 import { expect } from 'chai';
 import { Logger } from '@hmcts/nodejs-logging';
 
+// @ts-ignore
 import * as remote from 'selenium-webdriver/remote';
 
 const logger = Logger.getLogger('dwpresponse.page.ts');
