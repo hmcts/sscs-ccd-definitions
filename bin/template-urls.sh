e9b188c5e6d207020b4a4ea7c259ac28e1af446e
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2024-04-11 11:23:41 +0100

    Remove references to unused sscs orchestrator (#1548)

--- bin/template-urls.sh
+++ bin/template-urls.sh
@@ -4,7 +4,6 @@ SHEETS_DIR=`pwd`/data/sheets
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's/http:\/\/em-ccd-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_EM_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
-  sed -i '' 's/http:\/\/sscs-ccd-callback-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
   sed -i '' 's/http:\/\/sscs-tribunals-api-aat.service.core-compute-aat.internal/\${CCD_DEF_TRIBUNALS_API_URL}/g' ${SHEETS_DIR}/*.json
   sed -i '' 's/http:\/\/sscs-tya-notif-aat.service.core-compute-aat.internal/\${CCD_DEF_TYA_NOTIFICATIONS_API_URL}/g' ${SHEETS_DIR}/*.json
   sed -i '' 's/http:\/\/sscs-bulk-scan-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_API_URL}/g' ${SHEETS_DIR}/*.json
@@ -12,7 +11,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's/https:\/\/sscs-tya-frontend-aat.service.core-compute-aat.internal/validate-surname/\${subscriptions.appellantSubscription.tya}/trackyourappeal/g' ${SHEETS_DIR}/*.json
 else
   sed -i 's/http:\/\/em-ccd-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_EM_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
-  sed -i 's/http:\/\/sscs-ccd-callback-orchestrator-aat.service.core-compute-aat.internal/\${CCD_DEF_SSCS_CCD_ORCHESTRATOR_URL}/g' ${SHEETS_DIR}/*.json
   sed -i 's/http:\/\/sscs-tribunals-api-aat.service.core-compute-aat.internal/\${CCD_DEF_TRIBUNALS_API_URL}/g' ${SHEETS_DIR}/*.json
   sed -i 's/http:\/\/sscs-tya-notif-aat.service.core-compute-aat.internal/\${CCD_DEF_TYA_NOTIFICATIONS_API_URL}/g' ${SHEETS_DIR}/*.json
   sed -i 's/http:\/\/sscs-bulk-scan-aat.service.core-compute-aat.internal/\${CCD_DEF_BULK_SCAN_API_URL}/g' ${SHEETS_DIR}/*.json
