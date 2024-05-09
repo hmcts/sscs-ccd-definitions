40053cc8bf9e5f31413a5d8bc0c4a3df947caa39
Pettedson John <pettedson.john1@hmcts.net>
2024-05-09 12:06:56 +0100

    chore: testing the reporting functionality.

--- functional-test/playwright.config.ts
+++ functional-test/playwright.config.ts
@@ -19,7 +19,7 @@ module.exports = defineConfig({
 
   /* Opt out of parallel tests on CI. */
   workers: process.env.FUNCTIONAL_TESTS_WORKERS ? 5 : undefined,
-  reporter: [[process.env.CI ? "html" : "list", { open: "never", outputDir: '../playwright-report/'}]],
+  reporter: [[process.env.CI ? "html" : "list", { open: "never", outputDir: '../test-results/smokeTest/'}]],
   use: {
     baseURL: urls.xuiUrl,
     trace: "on-first-retry",
