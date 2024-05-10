cb235fbd738c9bc1cb2bdf62bb30776eabdfadc1
Arunabha Chowdhury <90389022+ArunabhaChowdhury@users.noreply.github.com>
2024-05-10 14:58:54 +0100

    Added test for Listing Error event and updating verifyPageContent method.

--- functional-test/lib/steps.factory.ts
+++ functional-test/lib/steps.factory.ts
@@ -1,6 +1,7 @@
 import { test as stepsFactory } from '@playwright/test';
 import { Note } from '../fixtures/steps/note';
 import { ConfirmCaseLapsed } from '../fixtures/steps/confirm.case.lapsed';
+import { SendToAdmin } from '../fixtures/steps/send.to.admin';
 import {ListingError} from "../fixtures/steps/listing.error";
 import {use} from "chai";
 
@@ -8,6 +9,7 @@ import {use} from "chai";
 type MyFixtures = {
     addNoteSteps: Note
     confirmCaseLapsedSteps: ConfirmCaseLapsed
+    sendToAdminSteps: SendToAdmin
     listingErrorSteps: ListingError
 
 };
@@ -20,6 +22,10 @@ export const test =  stepsFactory.extend<MyFixtures>({
     confirmCaseLapsedSteps:async ({page}, use) => {
         const confirmCaseLapsedSteps = new ConfirmCaseLapsed(page);
         await use(confirmCaseLapsedSteps);
+    },
+    sendToAdminSteps:async ({page}, use) => {
+        const sendToAdminSteps = new SendToAdmin(page);
+        await use(sendToAdminSteps);
     },
     listingErrorSteps:async ({page}, use) =>{
         const listingErrorSteps = new ListingError(page);
