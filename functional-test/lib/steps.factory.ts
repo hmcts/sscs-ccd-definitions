26ff71c792d1a10399429a3dda2f43971dee875f
Derin Aya <106395889+derin-aya@users.noreply.github.com>
2024-05-13 09:19:44 +0100

    SSCSSI-278: Evidence Reminder case tests (#1571)
    * Add initial tests
    
    * Update tests for evidence reminder
    
    * Update tests for evidence reminder
    
    * Update steps for evidence reminder
    
    * Fix conflict
    
    * Fix conflicts
    
    * Readd URLs
    
    * functional-test/pages/common/homePage.ts
    
    ---------
    
    Co-authored-by: gokul-sol <gokul.sridharan@HMCTS.NET>

--- functional-test/lib/steps.factory.ts
+++ functional-test/lib/steps.factory.ts
@@ -1,6 +1,7 @@
 import { test as stepsFactory } from '@playwright/test';
 import { Note } from '../fixtures/steps/note';
 import { ConfirmCaseLapsed } from '../fixtures/steps/confirm.case.lapsed';
+import { EvidenceReminder } from '../fixtures/steps/evidence.reminder';
 import { AssociateCase } from '../fixtures/steps/associate-case';
 import { SendToAdmin } from '../fixtures/steps/send.to.admin';
 import {ListingError} from "../fixtures/steps/listing.error";
@@ -11,6 +12,7 @@ type MyFixtures = {
     addNoteSteps: Note
     associateCaseSteps: AssociateCase
     confirmCaseLapsedSteps: ConfirmCaseLapsed
+    evidenceReminderSteps: EvidenceReminder
     sendToAdminSteps: SendToAdmin
     listingErrorSteps: ListingError
 
@@ -28,6 +30,10 @@ export const test =  stepsFactory.extend<MyFixtures>({
     confirmCaseLapsedSteps:async ({page}, use) => {
         const confirmCaseLapsedSteps = new ConfirmCaseLapsed(page);
         await use(confirmCaseLapsedSteps);
+    },
+    evidenceReminderSteps:async ({page}, use) => {
+        const evidenceReminderSteps = new EvidenceReminder(page);
+        await use(evidenceReminderSteps);
     },
     sendToAdminSteps:async ({page}, use) => {
         const sendToAdminSteps = new SendToAdmin(page);
