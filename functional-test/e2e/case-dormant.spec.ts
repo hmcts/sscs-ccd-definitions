71c5e8814a465f84c967b17db914fa551ac0770f
Pettedson John <pettedson.john1@hmcts.net>
2024-05-17 09:32:33 +0100

    chore: skipping the dormant case test

--- functional-test/e2e/case-dormant.spec.ts
+++ functional-test/e2e/case-dormant.spec.ts
@@ -2,8 +2,8 @@ import { test } from "../lib/steps.factory";
 import {credentials} from "../config/config"
 import {getIDAMUserToken, getSSCSServiceToken, getIDAMUserID} from "../api/client/idam/idam.service";
 
-test.only("Test to Add a note to a case", async ({ addNoteSteps }) => {
+test.skip("Test to Add a note to a case", async ({ addNoteSteps }) => {
     let token :string = await getIDAMUserToken(credentials.caseWorker);
-    //await getSSCSServiceToken();
+    await getSSCSServiceToken();
     await getIDAMUserID(token);
 });
