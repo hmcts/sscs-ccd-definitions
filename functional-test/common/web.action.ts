8e124a64117082bb275a7c1cf8f77137fe03a1ee
Pettedson John <pettedson.john1@hmcts.net>
2024-06-03 18:42:30 +0100

    fix: added delays to the code instead of the spinner

--- functional-test/common/web.action.ts
+++ functional-test/common/web.action.ts
@@ -68,7 +68,6 @@ export class WebAction {
             .catch((error) => {
                 logger.error(`Button element is not present: ${error}`);
             });
-        //await this.delay(2000);
         //await this.page.isHidden('.spinner-container',{strict: true, timeout: 4000});
         //expect(this.page).toHaveURL('trigger/dwpUploadResponse/dwpUploadResponse1.0');
     }
