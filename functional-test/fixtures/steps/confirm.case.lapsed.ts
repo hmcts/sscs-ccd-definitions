727ac49dff5ac22de57140c00c80a8c18fe4c996
derin-aya <derinaya101@gmail.com>
2024-06-05 18:52:29 +0100

    rebase branch

--- functional-test/fixtures/steps/confirm.case.lapsed.ts
+++ functional-test/fixtures/steps/confirm.case.lapsed.ts
@@ -13,7 +13,7 @@ export class ConfirmCaseLapsed extends BaseStep {
 
     async performConfirmCaseLapsed(caseId: string) {
 
-        await this.loginAsCaseworkerUserWithoutCaseId(caseId);
+        await this.loginAsCaseworkerUserWithCaseId(caseId);
         await this.homePage.reloadPage();
         await this.homePage.chooseEvent('Confirm lapsed');
 
