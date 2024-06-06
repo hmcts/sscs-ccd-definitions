83cba09dfa1c49b8f53ca3da19f6b3ab419ffe0f
Pettedson John <pettedson.john1@hmcts.net>
2024-06-06 14:47:18 +0100

    fix: add delay before choosing the drop down option

--- functional-test/pages/common/homePage.ts
+++ functional-test/pages/common/homePage.ts
@@ -54,7 +54,7 @@ export class HomePage {
     }
 
     async chooseEvent(eventName: string): Promise<void> {
-
+        await this.delay(2000);
         await webActions.chooseOptionByLabel(this.nextStepDropDown, eventName);
         await this.delay(2000);
         await webActions.clickSubmitButton();
