b5d9a3091076b30c1ff0a0f3b15ae590b71f045d
Derin Aya <106395889+derin-aya@users.noreply.github.com>
2024-06-10 10:42:42 +0100

    Remove delay

--- functional-test/fixtures/steps/send.to.dormant.ts
+++ functional-test/fixtures/steps/send.to.dormant.ts
@@ -16,14 +16,12 @@ export class SendToDormant extends BaseStep {
     async performSendToDormant(caseId: string) {
         await this.loginUserWithCaseId(credentials.amCaseWorker, false, caseId);
         await this.homePage.chooseEvent('Admin - send to Dormant');
-        await this.homePage.delay(4000);
 
         await this.eventNameAndDescriptionPage.verifyPageContent("Admin - send to Dormant");
         await this.eventNameAndDescriptionPage.inputData(eventTestData.eventSummaryInput,
             eventTestData.eventDescriptionInput);
         await this.eventNameAndDescriptionPage.confirmSubmission();
-        
-        await this.homePage.delay(4000);
+
         await this.verifyHistoryTabDetails("Dormant", 'Admin - send to Dormant', 'Event Description for Automation Verification');
     }
 }
