b2b51056ac5821638f316dc696cb6a7e8ec1967b
Pavi <pva08@Pavithrans-MacBook-Pro.local>
2024-06-11 14:38:09 +0100

    use instances from base step in information received event method

--- functional-test/fixtures/steps/information.received.ts
+++ functional-test/fixtures/steps/information.received.ts
@@ -1,40 +1,29 @@
 import { Page } from '@playwright/test';
-import { HomePage } from '../../pages/common/homePage';
 import informationReceivedData from "../../pages/content/information.received_en.json";
-import { History } from '../../pages/tabs/history';
-import { InformationReceivedPage } from "../../pages/information.received.page";
 import eventTestData from "../../pages/content/event.name.event.description_en.json";
-import { EventNameEventDescriptionPage } from "../../pages/common/event.name.event.description";
+import { BaseStep } from './base';
 
-export class InformationReceived {
+export class InformationReceived extends BaseStep {
 
     readonly page: Page;
 
     constructor(page: Page) {
+        super(page);
         this.page = page;
     }
 
     async performInformationReceivedEvent() {
 
-        let informationReceivedPage = new InformationReceivedPage(this.page);
-        let eventNameAndDescriptionPage = new EventNameEventDescriptionPage(this.page);
+        await this.informationReceivedPage.verifyPageContent();
+        await this.informationReceivedPage.selectReviewState(informationReceivedData.informationReceivedReviewStateSelectValue);
+        await this.informationReceivedPage.confirmSubmission();
 
-        await informationReceivedPage.verifyPageContent();
-        await informationReceivedPage.selectReviewState(informationReceivedData.informationReceivedReviewStateSelectValue);
-        await informationReceivedPage.confirmSubmission();
-
-        await eventNameAndDescriptionPage.inputData(eventTestData.eventSummaryInput,
+        await this.eventNameAndDescriptionPage.verifyPageContent(informationReceivedData.informationReceivedCaption);
+        await this.eventNameAndDescriptionPage.inputData(eventTestData.eventSummaryInput,
             eventTestData.eventDescriptionInput);
-        await eventNameAndDescriptionPage.confirmSubmission();
+        await this.eventNameAndDescriptionPage.confirmSubmission();
 
-        let homePage = new HomePage(this.page);
-        await homePage.navigateToTab("History");
-
-        let historyTab = new History(this.page);
-        await historyTab.verifyPageContentByKeyValue('Event', 'Information received');
-        await historyTab.verifyPageContentByKeyValue('Summary', 'Event Summary for Automation');
-        await historyTab.verifyPageContentByKeyValue('Comment', 'Event Description for Automation Verification');
-        await historyTab.verifyEventCompleted("Information received");
+        await this.verifyHistoryTabDetails('With FTA', 'Information received', eventTestData.eventDescriptionInput);
     }
 
 }
