8814f933859067c9e3aa0d9f3271a55ee4d007b7
Pettedson John <pettedson.john1@hmcts.net>
2024-06-12 15:22:46 +0100

    fix: broken test case-dormant in the pipeline

--- functional-test/e2e/case-dormant.spec.ts
+++ functional-test/e2e/case-dormant.spec.ts
@@ -1,14 +1,14 @@
 import {test} from "../lib/steps.factory";
 import {credentials} from "../config/config"
-import {accessToken, getSSCSServiceToken, getIDAMUserID} from "../api/client/idam/idam.service";
+import {accessToken, getSSCSServiceToken,accessId} from "../api/client/idam/idam.service";
 import performEventOnCase from "../api/client/sscs/factory/appeal.update.factory";
 import createCaseBasedOnCaseType from "../api/client/sscs/factory/appeal.type.factory";
 
-test.only("Test to Add a note to a case", async ({addNoteSteps}) => {
+test("Test to Add a note to a case", async ({addNoteSteps}) => {
     let token: string = await accessToken(credentials.caseWorker);
     console.log("The value of the IDAM Token : "+token);
     let serviceToken: string = await getSSCSServiceToken();
-    let userId: string = await getIDAMUserID(token);
+    let userId: string = await accessId(token);
     let pipCaseId = await createCaseBasedOnCaseType("CHILDSUPPORT");
     await performEventOnCase(token.trim(),
         serviceToken.trim(), userId.trim(),
