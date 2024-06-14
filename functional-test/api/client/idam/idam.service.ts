8f308f8e6294c4bfee0221c14ed355a06a38c8ce
pats-john <13101669+pats-john@users.noreply.github.com>
2024-06-14 07:20:25 +0100

    Add Caching (#1614)
    * chore: added the caching code
    
    * feature: cache initial version implemented
    
    * chore: refactor of the exisitng event token
    
    * fix: broken test case-dormant in the pipeline
    
    * fix: fixed the caching issue with the user
    
    * chore: fix the values of the payload
    
    * fix: getting the pipeline working for the caching method
    
    * fix: adding interim test
    
    * feat:add the caching to the api access layer
    
    * fix: removed the only in the case-dormant test
    
    * fix: npm audit fix...
    
    * fix: test that is struggling with the caching of the tokens
    
    * fix: adding the delete on expire flag.
    
    ---------
    
    Co-authored-by: Pettedson John <pettedson.john1@hmcts.net>

--- functional-test/api/client/idam/idam.service.ts
+++ functional-test/api/client/idam/idam.service.ts
@@ -2,9 +2,47 @@
 import {request} from '@playwright/test';
 import {urls, credentials, resources} from '../../../config/config';
 import logger from '../../../utils/loggerUtil';
+const NodeCache = require('node-cache');
+//Idam access token expires for every 8 hrs
+const tokenIDCache = new NodeCache({ stdTTL: 25200, checkperiod: 1800, maxKeys: 3, deleteOnExpire: true});
 
+export async function accessToken(user) {
+    console.log('User logged in', user.email);
+    if (tokenIDCache.get(user.email) != null) {
+        console.log('User access token coming from cache', user.email);
+        return tokenIDCache.get(user.email);
+    } else {
+        if (user.email && user.password) {
+            const accessToken = await getIDAMUserToken(user);
+            tokenIDCache.set(user.email, accessToken);
+            console.log('user access token coming from idam', user.email);
+            return accessToken;
+        } else {
+            console.log('******* Missing user details. Cannot get access token ******');
+        }
+    }
+}
 
-export async function getIDAMUserToken(user) {
+export async function accessId(user) {
+    let user_id_key = user.email + "_id"
+    console.log('The user email for getting the user id', user.email);
+    if (tokenIDCache.get(user_id_key) != null) {
+        console.log('User access token coming from cache', user_id_key);
+        return tokenIDCache.get(user_id_key);
+    } else {
+        if (user.email && user.password) {
+            const idamToken = await accessToken(user);
+            const userId = await getIDAMUserID(idamToken);
+            tokenIDCache.set(user_id_key, userId);
+            console.log('user id coming from idam', user_id_key);
+            return userId;
+        } else {
+            console.log('*******Missing user details. Cannot get the user id******');
+        }
+    }
+}
+
+async function getIDAMUserToken(user) {
     const scope = 'openid profile roles';
     const grantType = 'password';
     const idamTokenPath = '/o/token';
--- functional-test/api/client/sscs/appeal.event.ts
+++ functional-test/api/client/sscs/appeal.event.ts
@@ -1,15 +1,16 @@
 /* eslint-disable complexity */
 import logger from '../../../utils/loggerUtil';
-import {getIDAMUserID, getIDAMUserToken, getSSCSServiceToken} from "../idam/idam.service";
+import {accessId, accessToken, getSSCSServiceToken} from "../idam/idam.service";
 import {credentials} from "../../../config/config";
-import performEventOnCase from "./factory/appeal.update.factory";
+import {performEventOnCaseWithEmptyBody} from "./factory/appeal.update.factory";
 
 export default async function performAppealDormantOnCase(caseId: string) {
-    let token: string = await getIDAMUserToken(credentials.caseWorker);
+    let token: string = await accessToken(credentials.caseWorker);
     console.log("The value of the IDAM Token : "+token);
     let serviceToken: string = await getSSCSServiceToken();
-    let userId: string = await getIDAMUserID(token);
-    await performEventOnCase(token.trim(),
+    let userId: string = await accessId(credentials.caseWorker);
+    await new Promise(f => setTimeout(f, 3000));
+    await performEventOnCaseWithEmptyBody(token.trim(),
         serviceToken.trim(), userId.trim(),
         'SSCS','Benefit',
         caseId.trim(),'appealDormant')
