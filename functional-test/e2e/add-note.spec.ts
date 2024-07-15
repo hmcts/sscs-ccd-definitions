967055e41eec5f2ae1c4127674c460a10adfc570
gokul-sol <gokul.sridharan@HMCTS.NET>
2024-07-15 13:15:52 +0100

    tag tests to run in pipeline

--- functional-test/e2e/add-note.spec.ts
+++ functional-test/e2e/add-note.spec.ts
@@ -8,7 +8,7 @@ test.beforeAll("Case has to be Created", async () => {
     caseId = await createCaseBasedOnCaseType('PIP');
 });
 
-test("Test to Add a note to a case", {tag: '@pipeline'},
+test("Test to Add a note to a case",
     async ({addNoteSteps}) => {
     await addNoteSteps.performAddANote(caseId);
 });
