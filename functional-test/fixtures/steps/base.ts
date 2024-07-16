6c74f2b9ad75a97378cf0f0dbade2d4acc460054
pavihmcts <110388263+pavihmcts@users.noreply.github.com>
2024-07-16 09:22:27 +0100

    [SSCSSI-279]: add Review Reinstatement Request - Judge work allocation task tests (#1652)
    * add review reinstatement request task tests
    
    * update reinstatement spec file
    
    * fix urgent hearing request tests
    
    * rearrange reinstatement wa tests
    
    ---------
    
    Co-authored-by: Pavi <pva08@Pavithrans-MacBook-Pro.local>

--- functional-test/fixtures/steps/base.ts
+++ functional-test/fixtures/steps/base.ts
@@ -36,6 +36,7 @@ import { SupplementaryResponsePage } from "../../pages/supplementary.response.pa
 import { UploadDocumentFurtherEvidencePage } from '../../pages/upload.document.further.evidence.page';
 import { UpdateLanguagePreferencePage } from '../../pages/update.language.preference.page';
 import { Welsh } from '../../pages/tabs/welsh';
+import { AmendInterlocReviewStatePage } from '../../pages/amend.interloc.review.state.page';
 import { ReviewPHEPage } from '../../pages/review.phe.page';
 import { ListingRequirements } from '../../pages/tabs/listing.requirements';
 import { UpdateUCBPage } from '../../pages/update.ucb.page';
@@ -81,6 +82,7 @@ export abstract class BaseStep {
   protected supplementaryResponsePage: SupplementaryResponsePage;
   protected uploadDocumentFurtherEvidencePage: UploadDocumentFurtherEvidencePage;
   protected updateLanguagePreferencePage: UpdateLanguagePreferencePage;
+  protected amendInterlocReviewStatePage: AmendInterlocReviewStatePage;
   protected reviewPHEPage: ReviewPHEPage;
   protected listingRequirementsTab: ListingRequirements;
   protected updateUCBPage: UpdateUCBPage;
@@ -125,6 +127,7 @@ export abstract class BaseStep {
         this.supplementaryResponsePage = new SupplementaryResponsePage(this.page);
         this.uploadDocumentFurtherEvidencePage = new UploadDocumentFurtherEvidencePage(this.page);
         this.updateLanguagePreferencePage = new UpdateLanguagePreferencePage(this.page);
+        this.amendInterlocReviewStatePage = new AmendInterlocReviewStatePage(this.page);
         this.reviewPHEPage = new ReviewPHEPage(this.page);
         this.listingRequirementsTab = new ListingRequirements(this.page);
         this.updateUCBPage = new UpdateUCBPage(this.page);
