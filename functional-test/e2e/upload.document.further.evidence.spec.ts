40fd2f25416c56e09248fd411e4b3ed4f4437cae
Pavi <pva08@Pavithrans-MacBook-Pro.local>
2024-07-19 22:53:04 +0100

    fix failing work allocation tests

--- functional-test/e2e/upload.document.further.evidence.spec.ts
+++ functional-test/e2e/upload.document.further.evidence.spec.ts
@@ -3,7 +3,7 @@ import createCaseBasedOnCaseType from '../api/client/sscs/factory/appeal.type.fa
 import performAppealDormantOnCase from '../api/client/sscs/appeal.event';
 
 
-test.describe.serial('WA - Action Unprocessed Correspondence CTSC task initiation and completion tests', {
+test.describe.serial('WA - Upload document FE - Action Unprocessed Correspondence CTSC task initiation and completion tests', {
     tag: '@work-allocation'
 }, async () => {
 
@@ -46,14 +46,13 @@ test.describe.serial('WA - Action Unprocessed Correspondence CTSC task initiatio
 });
 
 
-test.describe('WA - Action Unprocessed Correspondence CTSC task cancellation', {
+test.describe.serial('WA - Upload document FE - Action Unprocessed Correspondence CTSC task cancellation', {
     tag: '@work-allocation'
 }, async() => {
 
     let caseId : string;
     
     test.beforeAll('Create case', async () => {
-        test.slow();
         caseId = await createCaseBasedOnCaseType('PIP');
     });
 
