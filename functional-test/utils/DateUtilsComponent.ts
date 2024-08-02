a57e5bcc2d84c54a41272692ca1f7b5553cd6b89
gokul <69240498+gokul-sol@users.noreply.github.com>
2024-08-02 14:33:49 +0100

    Create hearing test code (#1674)
    * Create hearing test code
    
    * cancellation test
    
    * cancellation tests
    
    * suppress yarn warnings
    
    * add amend test code
    
    * dynamically set weekdays for hearings
    
    * update date value
    
    ---------
    
    Co-authored-by: gokul-sol <gokul.sridharan@HMCTS.NET>

--- functional-test/utils/DateUtilsComponent.ts
+++ functional-test/utils/DateUtilsComponent.ts
@@ -160,6 +160,22 @@ class DateUtilsComponent {
     return weekend;
   }
 
+  static addWeekdays(startDate: Date, days: number): Date {
+    let count = 0;
+    let currentDate = new Date(startDate);
+
+    while (count < days) {
+        currentDate.setDate(currentDate.getDate() + 1);
+        const dayOfWeek = currentDate.getDay();
+        if (dayOfWeek !== 0 && dayOfWeek !== 6) {
+            count++;
+        }
+    }
+
+    return currentDate;
+}
+
+
 }
 
 const fourWeeksFroToday = DateUtilsComponent.rollDateToCertainWeeks(4);
