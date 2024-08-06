096425e7fe64071b690b4b8bc45124fd88b5d538
pavihmcts <110388263+pavihmcts@users.noreply.github.com>
2024-08-06 11:30:27 +0100

    [SSCSSI-281]: add Review PHE Request work allocation task tests (#1686)
    * add review PHE request wa task tests
    
    * update review PHE wa task tests
    
    ---------
    
    Co-authored-by: Pavi <pva08@Pavithrans-MacBook-Pro.local>

--- functional-test/e2e/urgent-hearing.spec.ts
+++ functional-test/e2e/urgent-hearing.spec.ts
@@ -2,9 +2,10 @@ import { test } from "../lib/steps.factory";
 import createCaseBasedOnCaseType from "../api/client/sscs/factory/appeal.type.factory";
 import performAppealDormantOnCase from "../api/client/sscs/appeal.event";
 
-let caseId : string;
 
 test.describe("Urgent hearing test", {tag: '@pipeline'}, async() => {
+
+    let caseId : string;
 
     test.beforeEach("Case has to be Created", async () => {
         caseId = await createCaseBasedOnCaseType('PIP');
