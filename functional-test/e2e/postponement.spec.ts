b249fe2845eb487da2948d52acc0d2930a2e5025
Pettedson John <pettedson.john1@hmcts.net>
2024-08-06 16:20:17 +0100

    test: testing the pipeline with all the tests

--- functional-test/e2e/postponement.spec.ts
+++ functional-test/e2e/postponement.spec.ts
@@ -7,17 +7,17 @@ let caseId : string;
 
 test.describe("Postponement Request test", {tag: '@wip'}, async() => {
 
-    test(" Hearing Route as Gaps with a Grant Option", async ({ postponementSteps }) => {
+    test("Hearing Route as Gaps with a Grant Option", async ({ postponementSteps }) => {
         test.slow();
         await postponementSteps.postponeAListAssistCaseWithAPostponement('Grant Postponement');
     });
 
-    test(" Hearing Route as Gaps with a Refuse Option", async ({ postponementSteps }) => {
+    test("Hearing Route as Gaps with a Refuse Option", async ({ postponementSteps }) => {
         test.slow();
         await postponementSteps.postponeAListAssistCaseWithAPostponement('Refuse Postponement');
     });
 
-    test(" Hearing Route as Gaps with a Send to Judge Option", async ({ postponementSteps }) => {
+    test("Hearing Route as Gaps with a Send to Judge Option", async ({ postponementSteps }) => {
         test.slow();
         await postponementSteps.postponeAListAssistCaseWithAPostponement('Send to Judge');
     });
