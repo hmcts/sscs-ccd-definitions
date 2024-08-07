cb7ec04e7a502dec6a7e2ce3e58a02eb50a9d1de
Pettedson John <pettedson.john1@hmcts.net>
2024-08-07 11:36:44 +0100

    test(fix): tagged the postponement tests

--- functional-test/e2e/postponement.spec.ts
+++ functional-test/e2e/postponement.spec.ts
@@ -5,7 +5,7 @@ import performAppealDormantOnCase from "../api/client/sscs/appeal.event";
 let caseId : string;
 
 
-test.describe("Postponement Request test", {tag: '@wip'}, async() => {
+test.describe("Postponement Request test", {tag: '@pipeline'}, async() => {
 
     test("Hearing Route as Gaps with a Grant Option", async ({ postponementSteps }) => {
         test.slow();
