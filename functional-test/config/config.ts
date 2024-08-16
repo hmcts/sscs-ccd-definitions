6ed026a3a505707fa7379d1e3da860581c4c8ce2
gokul-sol <gokul.sridharan@HMCTS.NET>
2024-08-16 12:25:05 +0100

    wip preview test code

--- functional-test/config/config.ts
+++ functional-test/config/config.ts
@@ -2,8 +2,8 @@ import path from "path";
 
 export const urls = {
 
-  tribunalsApiUri: process.env.TRIBUNALS_API_URL || "http://sscs-tribunals-api-aat.service.core-compute-aat.internal",
-  xuiUrl: process.env.SSCS_XUI_URL || 'https://manage-case.aat.platform.hmcts.net',
+  tribunalsApiUri: process.env.TRIBUNALS_API_URL || '',
+  xuiUrl: process.env.SSCS_XUI_URL || '',
   idamUrl : process.env.IDAM_URL || 'https://idam-api.aat.platform.hmcts.net',
   s2sUrl : process.env.S2S_URL || 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
   ccdApiUrl : process.env.CCD_API_URL || 'http://ccd-data-store-api-aat.service.core-compute-aat.internal'
