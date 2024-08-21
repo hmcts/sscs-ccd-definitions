42a897546c3798ef915c1d7e7cc0357d10191007
gokul-sol <gokul.sridharan@HMCTS.NET>
2024-08-21 22:50:26 +0100

    Add conditional logic to pick hearings tests

--- functional-test/e2e/hearing-request.spec.ts
+++ functional-test/e2e/hearing-request.spec.ts
@@ -7,7 +7,7 @@ let caseId : string;
 
 test.describe("Create a new hearing for an List assist case", {tag: '@pipeline'}, async() => {
     
-    test("Trigger a new hearing & cancellation for DLA case", async ({ uploadResponseSteps, hearingSteps }) => {
+    test("Trigger a new hearing & cancellation for DLA case", {tag:'@hearings-test'}, async ({ uploadResponseSteps, hearingSteps }) => {
         caseId = await createCaseBasedOnCaseType('DLASANDL');
         await uploadResponseSteps.performUploadResponse(caseId, 'dla');
         await hearingSteps.verifyHearingIsTriggered(caseId, 'dla');
@@ -20,7 +20,7 @@ test.describe("Create a new hearing for an List assist case", {tag: '@pipeline'}
         await hearingSteps.verifyHearingIsTriggeredForUCCase();
     });
 
-    test("Trigger a new hearing for PIP case",async ({ uploadResponseSteps, hearingSteps, voidCaseSteps }) => {
+    test("Trigger a new hearing for PIP case", async ({ uploadResponseSteps, hearingSteps, voidCaseSteps }) => {
         caseId = await createCaseBasedOnCaseType('PIPREPSANDL');
         await uploadResponseSteps.performUploadResponse(caseId, 'pip');
         await hearingSteps.verifyHearingIsTriggered(caseId, 'pip');
