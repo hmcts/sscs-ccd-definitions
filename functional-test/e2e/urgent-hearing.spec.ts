82696e0e7f80d0ba4d6b2aaf3b4db9b1892e32c3
Pettedson John <pettedson.john1@hmcts.net>
2024-09-03 14:33:18 +0100

    test(dev): Urgent hearing for the Welsh Appeal

--- functional-test/e2e/urgent-hearing.spec.ts
+++ functional-test/e2e/urgent-hearing.spec.ts
@@ -21,7 +21,7 @@ test.describe("Urgent hearing test", async() => {
         await urgentHearingSteps.requestAndRefuseAnUrgentHearing(caseId);
     });
 
-    test.only("Welsh - Urgent hearing request", async ({ urgentHearingSteps }) => {
+    test("Welsh - Urgent hearing request", async ({ urgentHearingSteps }) => {
         test.slow();
         await urgentHearingSteps.requestAnUrgentHearingForAWelshCase();
     });
